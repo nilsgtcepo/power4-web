19cc7ef39b43775d9caaf3d92600085cd0294f2e
nilsgtcepo <nilsgtcepo@gmail.com>
2025-10-07 15:42:19 +0200

    Version2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module power4
 
 go 1.25.0
+
+require golang.org/x/net v0.44.0 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/Documents", func (w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
